d8410d12ef8c52f91452ec719a513cf90ebdb65c
shiramori629 <82037101+shiramori629@users.noreply.github.com>
2021-04-19 15:04:26 +1000

    Update Something.java

--- 2020Q2/Something.java
+++ 2020Q2/Something.java
@@ -49,4 +49,44 @@ public class Something {
 
 		return sum;
 	}
+	
+public static void main(String[] args) {
+    int m1 = method2(72, 36);
+    int m2 = method1(31, 16);
+    int m3 = method3(72, 31);
+    int m4 = method3(36, 16);
+    int s1 = someMethod(1, 1, 1, 2);
+    System.out.println(s1);
+    // 228 36 36 30 45 ttt
+    // 72 36 36 1 5 tff
+    // 0 1 1 1 2 fff
+
+
+    for (int n1 = 1; n1 < 100; n1++) {
+      for (int n2 = 1; n2 < 100; n2++) {
+        for (int n3 = 1; n3 < 100; n3++) {
+          for (int n4 = 1; n4 < 100; n4++) {
+            if (!(method2(n1, n2) == VALUE1) && !(method3(n1, n3) == method3(n2, n4))
+                && !(method1(n3, n4) == VALUE2)) {
+              System.out.println(n1);
+              System.out.println(n2);
+              System.out.println(n3);
+              System.out.println(n4);
+              return;
+            }
+
+
+          }
+        }
+      }
+
+    }
+    //
+    // System.out.println(m1);
+    // System.out.println(m2);
+    // System.out.println(m3);
+    // System.out.println(m4);
+
+
+  }
 }
