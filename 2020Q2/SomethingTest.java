c2f36f4c997ffc6aa8e20f24ab5b232b49f4d801
shiramori629 <82037101+shiramori629@users.noreply.github.com>
2021-04-19 15:01:32 +1000

    Add files via upload

---
+++ 2020Q2/SomethingTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+/**
+ *
+ * You are given a java class called Something, which has a method called
+ * someMethod. Please implement a minimum number of test cases for testing `someMethod`
+ * that are branch complete within `someMethod`. Write your test case(s) in test() method in
+ * `SomethingTest.java`. You are not allowed to alter the signatures of the given
+ * methods and the package structures of the given classes. Please upload the
+ * `SomethingTest.java` file to Wattle for marking.
+ *
+ */
+public class SomethingTest {
+
+	@Test
+	public void test() {
+		// Implement your test cases
+		// START YOUR CODE
+
+
+		// END YOUR CODE
+	}
+}
